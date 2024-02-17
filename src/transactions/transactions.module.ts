62386e56db1d1dd5a275c383213672e29d0a58e9
= <=>
2024-02-17 13:22:57 +0100

    add jwt to transaction module

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -3,10 +3,11 @@ import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Transaction } from 'src/schemas/transaction.model';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [SequelizeModule.forFeature([Transaction])],
-  providers: [TransactionsService],
+  providers: [TransactionsService, JwtService],
   controllers: [TransactionsController],
 })
 export class TransactionsModule {}
